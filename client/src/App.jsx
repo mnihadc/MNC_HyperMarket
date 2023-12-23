e546fdf428e23ab84211510bd8cff7b68ec742cf
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-12-23 16:03:12 +0530

    Created client side react and tailwind css & created routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
+import React from 'react';
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Profile from './Pages/Profile';
+import About from './Pages/About';
+import Cart from './Pages/Cart';
+import Order from './Pages/Order';
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>MNC_Shopping-Cart</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Order />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1 className='text-center text-yellow-500'>Home Page</h1>
+        </div>
+    )
+}
+
+export default Home
