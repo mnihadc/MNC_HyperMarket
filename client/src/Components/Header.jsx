a8d6acb0c402e57a1a47bf5d025db685a65f46f9
Muhammed Nihad C <mnihadc1777@gmail.com>
2023-12-30 18:01:25 +0530

    created Server and run
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
+
         <header className='fixed top-0 w-full' style={{ backgroundColor: '#fceed1' }}>
             <div className='max-w-6xl mx-auto p-3 flex justify-between items-center'>
                 <Link to='/'>
@@ -11,16 +12,17 @@ function Header() {
                         <span className='text-sm lg:text-base text-slate-600'>_Shopping-Cart</span>
                     </h1>
                 </Link>
-                <ul className='flex gap-4'>
+                <ul className='flex gap-3'>
                     <Link to='/'>
                         <li className='hover:underline'>Home</li>
                     </Link>
-                    <Link to='/about'>
-                        <li className='hover:underline'>About</li>
+                    <Link to='/sign-in'>
+                        <li className='hover:underline'>SignIn</li>
                     </Link>
                 </ul>
             </div>
         </header>
+
     );
 }
 
