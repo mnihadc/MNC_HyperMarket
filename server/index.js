8232ae0f21476a48019a04d1755d52b363f46609
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-01-02 09:59:34 +0530

    Connected database Mongodb atlas
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express()
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.log("error  in connection to mongodb", err);
+    })
 
+const app = express();
+app.use(express.json());
 app.listen(3000, () => {
-    console.log("Server is running on port 3000!");
-});
+    console.log('Server is running on port 3000!');
+})
