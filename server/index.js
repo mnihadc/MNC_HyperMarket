e515d4f9b11198e9a905f1c7f65157cd6c5d64c9
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-01-02 12:31:56 +0530

    Created  Auth SignUp route and controller function
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/auth', authRouter);
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 })
