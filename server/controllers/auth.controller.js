1c7ac9a3bc0afebf1ca3a317f2d9b646e11fd6bf
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-01-02 17:33:12 +0530

    created SignIn route and controller
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { handleError } from "../utils/error.js";
 
 export const signUp = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -8,6 +9,24 @@ export const signUp = async (req, res, next) => {
     try {
         await newUser.save()
         res.status(201).json("User created Successfully")
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(handleError(404, 'User is not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(handleError(404, 'Wrong Credentials!'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest)
     } catch (error) {
         next(error);
     }
