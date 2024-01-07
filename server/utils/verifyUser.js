381b8b2d1030f4ce7bf989d56b63b5de37ff9240
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-01-07 12:00:33 +0530

    complete update user functionality
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookie.access_token;
+    const token = req.cookies.access_token;
 
     if (!token) return next(errorHandler(401, 'Unauthorized'));
 
