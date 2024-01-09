0ff8adbd522132f91c9816653dc19c635a1a1891
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-01-09 20:09:02 +0530

    created SignOut route and Controller
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signIn, signUp, google } from '../controllers/auth.controller.js';
+import { signIn, signUp, google, signOut } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/sign-up', signUp);
 router.post('/sign-in', signIn);
 router.post('/google', google);
+router.get('/signout',signOut);
 
 export default router;
