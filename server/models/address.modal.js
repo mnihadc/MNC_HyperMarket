0a5ebc6af65b808cc4eb166d9835115c1a91ed5b
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-01-11 20:54:14 +0530

    created Address Modal schema mongoose
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

---
+++ server/models/address.modal.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const addressSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    deliveryAddress: {
+        type: String,
+        required: true,
+    },
+    pinCode: {
+        type: String,
+        required: true,
+    },
+    contact: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    landmark: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    userRef: {
+        type: String,
+        required: true,
+    },
+},
+    { timestamps: true }
+);
+
+const Address = mongoose.model('Address', addressSchema);
+
+export default Address;
