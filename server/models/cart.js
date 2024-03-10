04688b5446dec17d531d8d68aea644cd9b2c97e9
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-03-10 11:17:04 +0530

    created Cart Schema
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

---
+++ server/models/cart.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const cartSchema = new mongoose.Schema({
+    productId: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+}, { timestamps: true });
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+export default Cart;
