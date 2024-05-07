745a8f82cc2087aa3b0e0230ca51cc5f6e40d608
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-05-07 17:27:58 +0530

    last update done
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
                 <div className='max-w-6xl mx-auto p-3 flex justify-between items-center'>
                     <Link to='/'>
                         <h1 className='font-semibold text-xl lg:text-4xl xl:text-4xl text-white' style={{ marginLeft: '1rem', sm: '2rem', lg: '4rem' }}>
-                            <span className='sm:text-xl lg:text-3xl xl:text-4xl text-green-500'>MNC</span>
+                            <span className='sm:text-xl lg:text-3xl xl:text-4xl text-green-500'>PNM</span>
                             <span className='text-sm lg:text-base text-slate-600'>
                                 {isAdmin ? '_HyperMarket ADMIN' : '_HyperMarket'}
                             </span>
