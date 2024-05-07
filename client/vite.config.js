659736c7bee5bbecdd0faee9d38ecf43bf501dbe
Muhammed Nihad C <mnihadc1777@gmail.com>
2024-05-07 18:18:18 +0530

    vite config
    Signed-off-by: Muhammed Nihad C <mnihadc1777@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   server: {
     proxy: {
       '/api': {
@@ -11,5 +12,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss(), autoprefixer()],
+};
